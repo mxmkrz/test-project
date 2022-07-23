c2964d9ac4f560ef1acba5ee7d4ccc830a5da116
Maksim Kuryzin <maksim.kuryzin.mku@gmail.com>
2022-07-23 13:04:45 +0300

    Initial commit

---
+++ src/main/kotlin/com/test/testik/TestikApplication.kt
@@ -0,0 +1,11 @@
+package com.test.testik
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestikApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestikApplication>(*args)
+}
---
+++ src/test/kotlin/com/test/testik/TestikApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.test.testik
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestikApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
